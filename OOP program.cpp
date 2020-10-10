48517c2efde65108a1ed5d1bb2d2e6fffdc81865
Davorin51 <72658502+Davorin51@users.noreply.github.com>
2020-10-10 15:53:42 +0200

    Create OOP program.cpp

---
+++ OOP program.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main(){
+	
+	int a;
+	int b;
+	
+	std::cout<<"Unesi a i b:";
+	
+	std::cin>>a;
+	std::cin>>b;
+	
+	int c = a + b;
+	
+	std::cout<<"Rezultat je: "<< c;
+	
+	return 0;
+	
+}
